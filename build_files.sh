bf1c85ace8a16fa3f85115a67ab009057386d722
khimani-yug <yugkhimani20@gmail.com>
2026-06-29 18:51:24 +0530

    Remove Vercel configuration files

--- build_files.sh
+++
@@ -1,6 +0,0 @@
-# build_files.sh
-echo "Building project..."
-python3 -m pip install -r requirements.txt --break-system-packages
-python3 manage.py collectstatic --noinput --clear
-python3 manage.py migrate --noinput
-echo "Build complete."
